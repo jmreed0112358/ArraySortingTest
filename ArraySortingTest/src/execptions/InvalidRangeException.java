baee8382553fb40a766a04ac46bc102ac20afb6d
Jeremy M. Reed <reeje76@gmail.com>
2014-03-10 08:52:01 -0600

    Removed TODO stubs, don't need extra code in the exception itself. Error handling should be done in the catch block.

--- ArraySortingTest/src/execptions/InvalidRangeException.java
+++ ArraySortingTest/src/execptions/InvalidRangeException.java
@@ -5,32 +5,27 @@ public class InvalidRangeException extends Exception
 
 	public InvalidRangeException( )
 	{
-		// TODO Auto-generated constructor stub
 	}
 
 	public InvalidRangeException( String message )
 	{
 		super( message );
-		// TODO Auto-generated constructor stub
 	}
 
 	public InvalidRangeException( Throwable cause )
 	{
 		super( cause );
-		// TODO Auto-generated constructor stub
 	}
 
 	public InvalidRangeException( String message, Throwable cause )
 	{
 		super( message, cause );
-		// TODO Auto-generated constructor stub
 	}
 
 	public InvalidRangeException( String message, Throwable cause,
 			boolean enableSuppression, boolean writableStackTrace )
 	{
 		super( message, cause, enableSuppression, writableStackTrace );
-		// TODO Auto-generated constructor stub
 	}
 
 }
